7b3dc3c181bb4c38af17031a7b6c59b3af928251
G Dev3 <gdev3.js@gmail.com>
2024-03-17 10:31:10 +0530

    Boolean Variable

---
+++ Beginner/04_Class/script.js
@@ -0,0 +1,20 @@
+let a = true;
+let b = false;
+
+console.log(typeof a);
+
+console.log(typeof b);
+
+console.log(a + b);
+
+console.log(true && false);
+
+console.log(true || false);
+
+console.log(!true);
+
+console.log((false && false) || true);
+
+a = true;
+
+console.log(!a);
