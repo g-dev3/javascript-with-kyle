24f1feafc2d82220eba8f04ae2fedb8c6cc6ff4d
G Dev3 <gdev3.js@gmail.com>
2024-03-17 10:00:02 +0530

    Number Variable

---
+++ Beginner/02_Class/script.js
@@ -0,0 +1,24 @@
+let favoriteNumber = 3;
+
+console.log(favoriteNumber);
+favoriteNumber = 4;
+
+console.log(favoriteNumber);
+
+let a, b, c;
+
+a = 1;
+b = 2;
+c = 3;
+
+console.log(a, b, c);
+
+a = 3;
+
+console.log(a);
+
+let d = a;
+
+console.log(d);
+
+
