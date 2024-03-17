a62a83d4b83a57356d5ddaa16c702d7b255ac43e
G Dev3 <gdev3.js@gmail.com>
2024-03-17 10:40:49 +0530

    Null and Undefined

---
+++ Beginner/05_Class/script.js
@@ -0,0 +1,19 @@
+let a = undefined;
+
+let b = null;
+
+console.log(a);
+
+console.log(b);
+
+console.log(typeof a);
+
+console.log(typeof b);
+
+let c;
+
+console.log(typeof c);
+
+let d = null;
+
+console.log(d);
