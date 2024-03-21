c8e67bb49ca7580c74431ab689836ed926db3c90
G Dev3 <gdev3.js@gmail.com>
2024-03-21 22:30:15 +0530

    Comparison oprator on different datatype

---
+++ Beginner/06_Class/script.js
@@ -0,0 +1,61 @@
+let a = 1;
+let b = 2;
+
+let result = a == b;
+
+result = a > b;
+
+result = a < b;
+
+result = a != b;
+
+result = a >= b;
+
+result = a <= b;
+
+let a1 = "a";
+let b1 = "a";
+
+result = a1 > b1;
+
+result = a1 == b1;
+
+result = a1 != b1;
+
+result = a1 < b1;
+
+result = a1 <= b1;
+
+result = a1 >= b1;
+
+let a2 = true;
+
+let b2 = false;
+
+result = a2 < b2;
+
+result = a2 > b2;
+
+result = a2 == b2;
+
+let a3 = null;
+
+let b3 = undefined;
+
+result = a3 == b3;
+
+result = a3 > b3;
+
+result = a3 < b3;
+
+result = a3 <= b3;
+
+result = a3 >= b3;
+
+let c = null;
+
+result = a3 >= c;
+
+result = a3 != c;
+
+console.log(result);
