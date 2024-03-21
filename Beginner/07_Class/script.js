7a8a0084e9469da26579beb42a86e45a3b6c8960
G Dev3 <gdev3.js@gmail.com>
2024-03-21 23:11:48 +0530

    Garbage collection and Comments

---
+++ Beginner/07_Class/script.js
@@ -0,0 +1,20 @@
+// This is a variable
+let a = 1;
+// Printed Value
+console.log(a);
+// This is second Variable
+let b = 2;
+/* When if second variable will print then the garbage collection will remove the first Variable from memory so with the 
+help of garbage collection it run fast memory and consume less.
+*/
+console.log(b);
+
+// ^ -- Example of Garbage collection
+
+// Single line Comment
+
+/* This is 
+Multiple 
+line 
+Comment
+*/
