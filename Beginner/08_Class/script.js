56448611aa8eb678c21e124788719643c9ea6efa
G Dev3 <gdev3.js@gmail.com>
2024-03-22 23:53:01 +0530

    return in functions

--- Beginner/08_Class/script.js
+++ Beginner/08_Class/script.js
@@ -3,3 +3,19 @@ function hello() {
 }
 
 hello();
+
+function sum(a, b) {
+  console.log(a + b);
+}
+
+sum(2, 1);
+
+// Return function
+
+function ssum(a, b = 0) {
+  return a + b;
+}
+
+let result = ssum(12);
+
+console.log(result);
