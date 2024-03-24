1642492263fd232d249b19d9c23f240ef3124f46
G Dev3 <gdev3.js@gmail.com>
2024-03-24 22:12:17 +0530

    passing function project

---
+++ Beginner/09_Class/script.js
@@ -0,0 +1,23 @@
+function name(name) {
+  console.log(name);
+}
+
+function argu(x) {
+  x("hello World");
+}
+
+argu(name);
+name("Gopal");
+
+//  practical
+
+function sayhi() {
+  return "Hello ";
+}
+
+function WelCome(name, sayhi) {
+  return console.log(sayhi + name);
+}
+
+let result = WelCome("Gopal", sayhi());
+result2 = WelCome("Sunil", sayhi());
