c85770ec6bbd74ce786af868f690f422bd797e31
G Dev3 <gdev3.js@gmail.com>
2024-03-26 08:35:14 +0530

    Callback function complete knowlage

--- Beginner/09_Class/script.js
+++ Beginner/09_Class/script.js
@@ -19,12 +19,44 @@ function WelCome(name, sayhi) {
   return console.log(sayhi + name);
 }
 
-let result = WelCome("Gopal", sayhi());
-result2 = WelCome("Sunil", sayhi());
 
-function printName(name, callback) {
-  console.log(callback);
-  callback("hello " + name);
+
+// repeat practice
+
+function printVarible(varible) {
+  console.log(varible);
 }
 
-printName("gopal", sayhi());
+// printVarible("hii");
+
+// console.log(printVarible)
+
+function func(x) {
+  x("hello World");
+}
+
+func(printVarible);
+
+function sumCallback(a, b, callback) {
+  callback(a + b);
+}
+
+function handleSum(sum) {
+  console.log(sum);
+}
+
+sumCallback(1, 2, handleSum);
+
+
+// one method
+function newfun(name, callBack) {
+  callBack("Hello " + name);
+}
+
+newfun("Gopal", printVarible);
+
+// Second method
+
+newfun("kyle", function(variable){
+  console.log(variable);
+})
