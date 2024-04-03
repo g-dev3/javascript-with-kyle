1d1bdbc47ea7b75ded1d6bf0fe8744039807d865
G Dev3 <gdev3.js@gmail.com>
2024-04-03 09:09:05 +0530

    Type coercion implicit and explicit

---
+++ Beginner/16_Class/script.js
@@ -0,0 +1,20 @@
+let a = "10";
+let b = "13.34";
+
+console.log(typeof parseInt(a));
+console.log(typeof a);
+
+console.log( typeof parseFloat(b));
+console.log(typeof b);
+
+let c = "hello" + 1
+
+console.log(typeof c)
+
+let d = 2 + "3"
+
+console.log(typeof parseInt(d))
+
+let e = "34"
+
+console.log(e.toString())
