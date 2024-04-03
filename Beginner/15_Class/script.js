e4f76fae49548e6b4d018da1c7ce1b77987bf94b
G Dev3 <gdev3.js@gmail.com>
2024-04-03 08:40:09 +0530

    Var

---
+++ Beginner/15_Class/script.js
@@ -0,0 +1,36 @@
+// Var
+
+{
+  //   let a = 12;
+  var a = 12;
+
+  // console.log(a)
+}
+
+console.log(a);
+
+// its innerworking working look like
+
+console.log(c);
+
+var c = "G";
+
+console.log(c);
+
+// inner Working
+
+/*
+
+var c = undefined;
+
+console.log(c)  // its give result as a undefined
+
+var c = "G" // in here its treat like c = "G"
+
+console.log(c) // in this situation its work as a c = "G"
+
+*/
+
+//  one more think in var we can redefine var one same variable
+
+var c = 10; // in this situation its change their value "G" to 10
