b9fc246b8012fedda80a80f508ecb1226f251053
G Dev3 <gdev3.js@gmail.com>
2024-04-23 22:36:22 +0530

    Type coersion in logical oprator

--- Beginner/16_Class/script.js
+++ Beginner/16_Class/script.js
@@ -4,17 +4,24 @@ let b = "13.34";
 console.log(typeof parseInt(a));
 console.log(typeof a);
 
-console.log( typeof parseFloat(b));
+console.log(typeof parseFloat(b));
 console.log(typeof b);
 
-let c = "hello" + 1
+let c = "hello" + 1;
 
-console.log(typeof c)
+console.log(typeof c);
 
-let d = 2 + "3"
+let d = 2 + "3";
 
-console.log(typeof parseInt(d))
+console.log(typeof parseInt(d));
 
-let e = "34"
+let e = "34";
 
-console.log(e.toString())
+console.log(e.toString());
+
+let num1 = 1;
+let num2 = 3;
+
+let resultNum = a !== 3;
+
+console.log(resultNum);
