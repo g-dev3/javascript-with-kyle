49cd479708032624d3d37f43f0783a180aa11a1d
G Dev3 <gdev3.js@gmail.com>
2024-04-25 03:32:48 +0530

    Arrays

---
+++ Beginner/18_Class/script.js
@@ -0,0 +1,32 @@
+let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(a[3]);
+
+a.push(11);
+
+a.pop(10);
+
+console.log(a);
+
+let b = [
+  ["a", "B"],
+  [1, 2],
+];
+
+console.log(b[1][0]);
+
+let c = ["a", "b", "c", "d", "e"];
+
+console.log(c[2]);
+
+let d = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15],
+];
+
+console.log(d[0][3]);
+console.log(d[1][2]);
+console.log(d[2][0]);
+
+console.log(d.length)
